81c8839b622ec81efffc078a03f2e71102806590
Gilberto Freitas <gilberto.ademar@gmail.com>
2017-12-07 14:24:35 -0200

    add method private in mongo service

--- app/service/mongo_service.py
+++ app/service/mongo_service.py
@@ -9,15 +9,27 @@ mongo_wrapper = MongoWrapper()
 
 class MongoService(object):
     name_collection = 'keys'
+    db = None
+
+    def __db(self):
+        if self.db:
+            return self.db
+
+        self.db = MongoWrapper.connect(mongo_wrapper)
+        return self.db
 
     def get_one(self, id):
-        db = MongoWrapper.connect(mongo_wrapper)
+        db = self.__db()
         return db[self.name_collection].find_one({'id': id})
 
     def insert_one(self, key):
-        db = MongoWrapper.connect(mongo_wrapper)
+        db = self.__db()
         return db[self.name_collection].insert_one(key)
 
     def update_one(self, new_key):
-        db = MongoWrapper.connect(mongo_wrapper)
+        db = self.__db()
         return db[self.name_collection].update_one({'id': new_key['id']}, {'$set': new_key})
+
+    def delete_one(self, id):
+        db = self.__db()
+        return db[self.name_collection].delete_one({'id': id})
